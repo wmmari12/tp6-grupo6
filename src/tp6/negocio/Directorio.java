fc181c31b881454c82ec0723ca1605b6331512d2
wmmari12 <wmari12@gmail.com>
2023-05-02 17:09:29 -0300

    metodos

--- src/tp6/negocio/Directorio.java
+++ src/tp6/negocio/Directorio.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tp6.negocio;
 
-/**
- *
- * @author solmedina
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JOptionPane;
+
+
 public class Directorio {
+    
+    private HashMap<String, Cliente> registro= new HashMap<String, Cliente>();
+    
+
+    public boolean agregarCliente(String telefono, Cliente c){
+        
+        boolean existe=false;
+        
+        if(!registro.containsKey(telefono)){
+            
+            registro.put(telefono, c);
+            existe=true;
+        }else{
+            JOptionPane.showMessageDialog(null, "Cliente ya registrado");
+        }
+        return existe;
+    }
+    
+    public Cliente buscarCliente(String telefono) {
+        
+        Cliente c=null;
+        
+        if(registro.containsKey(telefono)){
+            
+           c=registro.get(telefono);     
+        }else{
+            JOptionPane.showMessageDialog(null, "Cliente no registrado");
+        }
+        return c;
+    }
+    
+//    buscarTeléfono() que en base a un apellido nos devuelve una lista con los nros de
+//teléfono asociados a dicho apellido.
+    public ArrayList<String> buscarTelefono(String apellido){
+        
+        ArrayList <String> lista=new ArrayList<String>();
+        
+        for (Map.Entry<String, Cliente> entry : registro.entrySet())
+        {
+            String key = entry.getKey();
+            Cliente value = entry.getValue();
+            
+        }
+                
+        return lista;
+    }
+    
+    
+    
+    
+    
+    
     
 }
