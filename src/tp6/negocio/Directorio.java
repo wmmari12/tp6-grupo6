8730a97584cf5685b50a2fd146aecd617d105414
Lucas Fernandez <lucasm2664789183@gmail.com>
2023-05-02 18:21:32 -0300

    asd

--- src/tp6/negocio/Directorio.java
+++ src/tp6/negocio/Directorio.java
@@ -3,7 +3,9 @@ package tp6.negocio;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import javax.swing.JOptionPane;
 
 
@@ -47,18 +49,30 @@ public class Directorio {
         
         for (Map.Entry<String, Cliente> entry : registro.entrySet())
         {
-            String key = entry.getKey();
-            Cliente value = entry.getValue();
-            
+
+            if(registro.containsValue(apellido)){
+                lista.add(entry.getKey());
+            }
         }
-                
         return lista;
     }
+   
+//    buscarClientes() que en base a una ciudad nos devuelve una lista con los Clientes 
+//    asociados a dicha ciudad.
     
+     public ArrayList<Cliente> buscarClientes(String ciudad){
+        
+        ArrayList <Cliente> lista=new ArrayList<Cliente>(); // lista de tipo cliente
+        Set <String> keys = registro.keySet();//lista Set que almacena las llaves
+        Iterator <String> it = keys.iterator(); // iterador para recorrer las keys
+        
+        while(it.hasNext()){ // verifica que exista un proximo
+            String keyActual = it.next(); //guarda la llave dnd esta posicionado
+            Cliente clienteActual = registro.get(keyActual);//guarda el cliente donde esta posicionado el it
+            
+            if(clienteActual.getCiudad().equals(ciudad))
+        }
+    }
     
-    
-    
-    
-    
-    
+    //borrarCliente() que en base al número de teléfono elimina al cliente del directorio.
 }
